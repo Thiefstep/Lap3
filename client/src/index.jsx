a52531d7818178b007332a1fe67bd3fec3229dc0
Tom Burzynski <tellur.beryl@gmail.com>
2023-05-30 20:24:08 +0100

    add providers to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthContextProvider } from './context/auth';
+import { FlashcardContextProvider } from './context/flashcard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Router>
-		<App />
+		<AuthContextProvider>
+			<FlashcardContextProvider>
+				<App />
+			</FlashcardContextProvider>
+		</AuthContextProvider>
 	</Router>
 );
