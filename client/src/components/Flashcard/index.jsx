9591489f2124bf3df5b5799608d959a9d198ef46
Thiefstep <matzz1@hotmail.co.uk>
2023-05-30 09:46:26 +0100

    Flashcard added

---
+++ client/src/components/Flashcard/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Flashcard = ({front, back}) => {
+
+    return(
+        <div className="flashcard">
+            <div className='card'>
+                <div className='front'>
+                    <p>{front}</p>
+                </div>
+                <div className='back'>
+                    <p>{back}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Flashcard
