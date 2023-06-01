c371f5737e1b242dee342dded2afb62d0020638d
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-06-01 12:13:30 +0100

    small changes

--- client/src/pages/CreatePage/index.jsx
+++ client/src/pages/CreatePage/index.jsx
@@ -1,111 +1,127 @@
-import React, { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { createFlashcards } from '../../actions';
-import { useFlashcards } from '../../hooks/useFlashcards';
-import { useAuth } from '../../hooks/useAuth';
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createFlashcards } from "../../actions";
+import { useFlashcards } from "../../hooks/useFlashcards";
+import { useAuth } from "../../hooks/useAuth";
 
 const CreatePage = () => {
-	const { dispatch } = useFlashcards();
-	const { user } = useAuth();
+  const { dispatch } = useFlashcards();
+  const { user } = useAuth();
 
-	const [frontSide, setFrontSide] = useState('');
-	const [category, setCategory] = useState('');
-	const [backSide, setBackSide] = useState('');
+  const [frontSide, setFrontSide] = useState("");
+  const [category, setCategory] = useState("");
+  const [backSide, setBackSide] = useState("");
 
-	const errorCreate = (error) =>
-		toast.error(error, {
-			position: 'top-center',
-			autoClose: 5000,
-			hideProgressBar: false,
-			closeOnClick: true,
-			pauseOnHover: true,
-			draggable: true,
-			progress: undefined,
-			theme: 'light',
-		});
+  const errorCreate = (error) =>
+    toast.error(error, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
 
-	const successCreate = (message) =>
-		toast(message, {
-			position: 'top-center',
-			autoClose: 5000,
-			hideProgressBar: false,
-			closeOnClick: true,
-			pauseOnHover: true,
-			draggable: true,
-			progress: undefined,
-			theme: 'light',
-		});
+  const successCreate = (message) =>
+    toast(message, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
 
-	const handleInputQuestion = (e) => {
-		setFrontSide(e.target.value);
-	};
-	const handleInputCategory = (e) => {
-		setCategory(e.target.value);
-	};
-	const handleInputAnswer = (e) => {
-		setBackSide(e.target.value);
-	};
+  const handleInputQuestion = (e) => {
+    setFrontSide(e.target.value);
+  };
+  const handleInputCategory = (e) => {
+    setCategory(e.target.value);
+  };
+  const handleInputAnswer = (e) => {
+    setBackSide(e.target.value);
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		const flashcard = { frontSide, backSide, category };
+    const flashcard = { frontSide, backSide, category };
 
-		const options = {
-			method: 'POST',
-			body: JSON.stringify(flashcard),
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${user.token}`,
-			},
-		};
+    const options = {
+      method: "POST",
+      body: JSON.stringify(flashcard),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
 
-		try {
-			const res = await fetch('http://localhost:3000/flashcards', options);
-			const data = await res.json();
+    try {
+      const res = await fetch("http://localhost:3000/flashcards", options);
+      const data = await res.json();
 
-			if (!res.ok) {
-				errorCreate(`${data.error}`);
-			}
+      if (!res.ok) {
+        errorCreate(`${data.error}`);
+      }
 
-			if (res.ok) {
-				setFrontSide('');
-				setBackSide('');
-				setCategory('');
+      if (res.ok) {
+        setFrontSide("");
+        setBackSide("");
+        setCategory("");
 
-				successCreate('Flash card has been created!');
-				dispatch(createFlashcards(data));
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+        successCreate("Flash card has been created!");
+        dispatch(createFlashcards(data));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	return (
-		<>
-			<form className="register" onSubmit={handleSubmit}>
-				<h1>Create Your Flashcard</h1>
-				<label htmlFor="frontSide">
-					<b>Question</b>
-				</label>
-				<input type="text" id="question" name="frontSide" onChange={handleInputQuestion} />
+  return (
+    <>
+      <form className="register" onSubmit={handleSubmit}>
+        <h1>Create Your Flashcard</h1>
+        <label htmlFor="frontSide">
+          <b>Question</b>
+        </label>
+        <input
+          type="text"
+          id="question"
+          name="frontSide"
+          onChange={handleInputQuestion}
+        />
 
-				<label htmlFor="category">
-					<b>Category</b>
-				</label>
-				<input type="text" id="category" name="category" onChange={handleInputCategory} />
+        <label htmlFor="category">
+          <b>Category</b>
+        </label>
+        <input
+          type="text"
+          id="category"
+          name="category"
+          onChange={handleInputCategory}
+        />
 
-				<label htmlFor="backSide">
-					<b>Answer</b>
-				</label>
-				<textarea id="answer" name="backSide" rows="5" cols="50" onChange={handleInputAnswer} />
+        <label htmlFor="backSide">
+          <b>Answer</b>
+        </label>
+        <textarea
+          id="answer"
+          name="backSide"
+          rows="5"
+          cols="50"
+          onChange={handleInputAnswer}
+        />
 
-				<button>Create</button>
-				<ToastContainer />
-			</form>
-		</>
-	);
+        <button>Create</button>
+        <ToastContainer />
+      </form>
+    </>
+  );
 };
 
 export default CreatePage;
--- client/src/pages/Flashcard/index.jsx
+++ client/src/pages/Flashcard/index.jsx
@@ -1,54 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { Card } from '../../components';
-import { useFlashcards } from '../../hooks/useFlashcards';
-import { useAuth } from '../../hooks/useAuth';
-import { setFlashcards } from '../../actions';
+import React, { useState, useEffect } from "react";
+import { Card } from "../../components";
+import { useFlashcards } from "../../hooks/useFlashcards";
+import { useAuth } from "../../hooks/useAuth";
+import { setFlashcards } from "../../actions";
 
 const Flashcard = () => {
-	const [curCardId, setCurCardId] = useState(1);
-	const { flashcards, dispatch } = useFlashcards();
-	const { user } = useAuth();
+  const [curCardId, setCurCardId] = useState(1);
+  const { flashcards, dispatch } = useFlashcards();
+  const { user } = useAuth();
 
-	useEffect(() => {
-		const fetchFlashcards = async () => {
-			try {
-				const res = await fetch('http://localhost:3000/flashcards', {
-					headers: {
-						Authorization: `Bearer ${user.token}`,
-					},
-				});
-				const data = await res.json();
+  useEffect(() => {
+    const fetchFlashcards = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/flashcards", {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        const data = await res.json();
 
-				if (res.ok) {
-					dispatch(setFlashcards(data));
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		fetchFlashcards();
-	}, [dispatch, user]);
+        if (res.ok) {
+          dispatch(setFlashcards(data));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchFlashcards();
+  }, [dispatch, user]);
 
-	const handleNextCard = () => {
-		setCurCardId((prevCardId) => prevCardId + 1);
-	};
+  const handleNextCard = () => {
+    setCurCardId((prevCardId) => prevCardId + 1);
+  };
 
-	const handlePreviousCard = () => {
-		if (curCardId > 1) {
-			setCurCardId((prevCardId) => prevCardId - 1);
-		}
-	};
+  const handlePreviousCard = () => {
+    if (curCardId > 1) {
+      setCurCardId((prevCardId) => prevCardId - 1);
+    }
+  };
 
-	return (
-		<div className="home">
-			{flashcards && flashcards.map((f) => <Card key={f._id} flashcard={f} />)}
+  return (
+    <div className="home">
+      {flashcards && flashcards.map((f) => <Card key={f._id} flashcard={f} />)}
 
-			<div className="flashbtn">
-				<button onClick={handlePreviousCard}>Previous</button>
-				<button onClick={handleNextCard}>Next</button>
-			</div>
-		</div>
-	);
+      <div className="flashbtn">
+        <button onClick={handlePreviousCard}>Previous</button>
+        <button onClick={handleNextCard}>Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default Flashcard;
