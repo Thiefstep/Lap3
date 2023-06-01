898665283760bf610ff75338496c435c5369e187
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-06-01 16:20:07 +0100

    update component index.jsx

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,2 +1,3 @@
-export { default as Card } from './Card';
-export { default as PageWrapper} from './PageWrapper';
+export { default as Card } from "./Card";
+export { default as PageWrapper } from "./PageWrapper";
+export { default as CardFilters } from "./CardFilters";
